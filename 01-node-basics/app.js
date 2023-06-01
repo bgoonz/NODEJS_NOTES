e0f9c4b74e3a6c4705d92cb7ae6222ac3226d1b8
Bryan C Guner <bryan.guner@gmail.com>
2023-06-01 10:30:03 -0400

    createServer

---
+++ 01-node-basics/app.js
@@ -0,0 +1,10 @@
+const http = require( 'http' );
+
+function rqListener( req, res ) {
+    
+}
+// the createServer method will execute the rqListener function for every request
+http.createServer( rqListener );
+
+
+
