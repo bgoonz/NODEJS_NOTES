1ea7ed16363431006a7044bc4a31a72b12dd7de0
Bryan C Guner <bryan.guner@gmail.com>
2023-06-01 12:20:07 -0400

    using response.write to output basic html document

--- 01-node-basics/app.js
+++ 01-node-basics/app.js
@@ -3,7 +3,10 @@ const http = require("http");
 const server = http.createServer((request, response) => {
     console.log( request.url, request.method, request.headers );
     response.setHeader( "Content-Type", "text/html" );
-    response.write( "<h2> Hello </h2>" );
+    response.write( '<html>' );
+    response.write( '<head><title>My First Page</title></head>' );
+    response.write( '<body><h1>Hello from my Node.js Server!</h1></body>' );
+    response.write( '</html>' );
 });
 
 server.listen(3000);
