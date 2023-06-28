f185bf1845bd40ff871cb7a1102dcff49657aafe
gunerbr <bryan.guner@gmail.com>
2023-06-28 16:40:21 -0400

    setting up for templating engines section

---
+++ 00-notes/practice/navigation/README.md
@@ -0,0 +1,9 @@
+# Navigation Challenge
+
+1. Create a npm project & install express /nodemon
+2. Create express.js app which serves two html files for the following routes:
+
+   - `/` - index.html
+   - `/users` - users.html
+
+3. Add some static (.js or .css) files to your project that should be required by at least one of your html files.
---
+++ 00-notes/practice/navigation/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname,'..' ,"views", "index.html"));
+});
+
+app.use("/users", (req, res) => {
+  res.sendFile(path.join(__dirname,'..', "views", "users.html"));
+});
+app.listen(3000);
