3dd4100d59e1f04f2081cd533078a87e1d25707a
Bryan C Guner <bryan.guner@gmail.com>
2023-10-17 12:11:12 -0400

    handlebars layout

--- 03-templating-engines/app.js
+++ 03-templating-engines/app.js
@@ -7,7 +7,14 @@ const expressHbs = require("express-handlebars");
 const app = express();
 // app.set("view engine", "pug");
 
-app.engine("hbs", expressHbs());
+app.engine(
+  "hbs",
+  expressHbs({
+    layoutsDir: "views/layouts/",
+    defaultLayout: "main-layout",
+    extname: "hbs",
+  }),
+);
 
 app.set("view engine", "hbs");
 const adminData = require("./routes/admin");
