8a0c83edd022346850efd8d87dd281caa1a2b540
Bryan C Guner <bryan.guner@gmail.com>
2023-10-19 10:32:12 -0400

    refactored fetchAl to use callback to fetch data for the Shop page

--- 04-mvc/models/product.js
+++ 04-mvc/models/product.js
@@ -1,30 +1,33 @@
-
-const fs = require('fs');
-const path = require('path');
-const rootDir = require('../util/path');
-
-
+const fs = require("fs");
+const path = require("path");
+const rootDir = require("../util/path");
 
 module.exports = class Product {
-    constructor(title){
-        this.title=title;
-    }
-    save(){
-        const filePath = path.join(rootDir, 'data', 'products.json'); 
-        fs.readFile(filePath, (error, fileContent)=>{
-            let products = [];
-            if(!error){
-                console.log(fileContent);
-                products = JSON.parse(fileContent);
-            }
-            products.push(this);
-            fs.writeFile(filePath, JSON.stringify(products), (error)=>{
-                console.log(error);
-            });
-        })
-    }
-    
-   static fetchAll(){
-        // return products;
-    }
-}
+  constructor(title) {
+    this.title = title;
+  }
+  save() {
+    const filePath = path.join(rootDir, "data", "products.json");
+    fs.readFile(filePath, (error, fileContent) => {
+      let products = [];
+      if (!error) {
+        console.log(fileContent);
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(filePath, JSON.stringify(products), (error) => {
+        console.log(error);
+      });
+    });
+  }
+
+  static  fetchAll(cb) {
+    const filePath = path.join(rootDir, "data", "products.json");
+     fs.readFile(filePath, (error, fileContent) => {
+      if (error) {
+         cb([]);
+      }
+      cb(JSON.parse(fileContent));
+    });
+  }
+};
--- README.md
+++ README.md
@@ -620,13 +620,17 @@ exports.getProducts = (req, res, next) => {
 
 ```js
 save(){
-        const filePath = path.join(pathUtil, 'data', 'products.json'); 
+        const filePath = path.join(rootDir, 'data', 'products.json'); 
         fs.readFile(filePath, (error, fileContent)=>{
             let products = [];
             if(!error){
+                console.log(fileContent);
                 products = JSON.parse(fileContent);
             }
             products.push(this);
+            fs.writeFile(filePath, JSON.stringify(products), (error)=>{
+                console.log(error);
+            });
         })
     }
 ```
