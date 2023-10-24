d0892c37e273fbd2366fd936df586896aa8cecf3
Bryan C Guner <bryan.guner@gmail.com>
2023-10-24 09:17:33 -0400

    updated admin controller and model and view to add new fields to add product form

--- 04-mvc/controllers/admin.js
+++ 04-mvc/controllers/admin.js
@@ -10,18 +10,22 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title);
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const description = req.body.description;
+  const price = req.body.price;
+  //the arguments here need to be in the same order as you wrote them in the constructor in the model
+  const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect("/");
 };
 
-
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render("admin/products", {
-          prods: products,
-          pageTitle: "Admin Products",
-          path: "/admin/products"
-        });
-      });
-}
+  Product.fetchAll((products) => {
+    res.render("admin/products", {
+      prods: products,
+      pageTitle: "Admin Products",
+      path: "/admin/products",
+    });
+  });
+};
--- 04-mvc/controllers/shop.js
+++ 04-mvc/controllers/shop.js
@@ -5,7 +5,7 @@ exports.getProducts = (req, res, next) => {
     res.render("shop/product-list", {
       prods: products,
       pageTitle: "All Products",
-      path: "/products"
+      path: "/products",
     });
   });
 };
@@ -15,7 +15,7 @@ exports.getIndex = (req, res, next) => {
     res.render("shop/index", {
       prods: products,
       pageTitle: "Shop",
-      path: "/"
+      path: "/",
     });
   });
 };
@@ -23,13 +23,13 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   res.render("shop/cart", {
     path: "/cart",
-    pageTitle: "Your Cart"
+    pageTitle: "Your Cart",
   });
 };
 
 exports.getCheckout = (req, res, next) => {
   res.render("shop/checkout", {
     path: "/checkout",
-    pageTitle: "Checkout"
+    pageTitle: "Checkout",
   });
 };
