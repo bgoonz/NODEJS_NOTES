13d20ecfdc2828623bba1526b44c8cb62e103ed0
Bryan C Guner <bryan.guner@gmail.com>
2024-03-11 14:24:19 -0400

    loading product detail data

--- 05-dynamic-routes/models/product.js
+++ 05-dynamic-routes/models/product.js
@@ -1,13 +1,9 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
-);
+const p = path.join(path.dirname(process.mainModule.filename), "data", "products.json");
 
-const getProductsFromFile = cb => {
+const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
@@ -26,10 +22,10 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString()
-    getProductsFromFile(products => {
+    this.id = Math.random().toString();
+    getProductsFromFile((products) => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
+      fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
       });
     });
@@ -37,5 +33,14 @@ module.exports = class Product {
 
   static fetchAll(cb) {
     getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile((products) => {
+      const product = products.find((prod) => {
+        return (prod.id = id);
+      });
+      cb(product)
+    });
   }
 };
